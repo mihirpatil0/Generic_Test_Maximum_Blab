a7f56d91fd18f152cbef7e5c1d04566dc95609fd
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 01:37:54 +0530

    Added maxmimumString function that computes maximum string amoungst all strings

--- GenericMaximum.java
+++ GenericMaximum.java
@@ -4,6 +4,7 @@ public class GenericMaximum
 {
 	static Integer maxInteger;
 	static Float maxFloat;
+	static String maxString;
 	
 	/**
 	 * @param a
@@ -49,22 +50,48 @@ public class GenericMaximum
 		return maxFloat;
 	}
 	
+	/**
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 * Finding max String value using compareTo() method.
+	 */
+	public static String maximumString(String a, String b, String c)
+	{
+		maxString = a;
+		
+		if(b.compareTo(maxString) > 0)
+		{
+			maxString = b;
+		}
+		if(c.compareTo(maxString) > 0)
+		{
+			maxString = c;
+		}
+		return maxString;
+	}
+	
 	
 	/**
 	 * @param args
-	 * Passing integer and float value to function.
+	 * Passing integer, float and String value to function.
 	 * Calling functions.
 	 * printing the maximum number as a result.
 	 */
 	public static void main(String[] args)
 	{
 		Integer intNumeOne = 15, intNumTwo = 55, intNumThree = 24;
-		Float floatNumOne = 218.13f, floatNumTwo = 555.453f, floatNumThree = 999.999f;  
+		Float floatNumOne = 218.13f, floatNumTwo = 555.453f, floatNumThree = 999.999f;
+		String stringOne = "Apple", stringTwo = "Peach", stringThree = "Banana";
 		
 		maximumInteger(intNumeOne, intNumTwo, intNumThree);
 		System.out.println("Maximum amoungst : " + intNumeOne + " " + intNumTwo + " " + intNumThree + " is : " + maxInteger + "\n");
 		
 		maximumFloat(floatNumOne, floatNumTwo, floatNumThree);
-		System.out.println("Maximum amoungst : " + floatNumOne + " " + floatNumTwo + " " + floatNumThree + " is : " + maxFloat);
+		System.out.println("Maximum amoungst : " + floatNumOne + " " + floatNumTwo + " " + floatNumThree + " is : " + maxFloat + "\n");
+		
+		maximumString(stringOne, stringTwo, stringThree);
+		System.out.println("Maximum amoungst : " + stringOne + " " + stringTwo + " " + stringThree + " is : " + maxString);
 	}
 }
