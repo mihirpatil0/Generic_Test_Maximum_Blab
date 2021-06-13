edb8a8e0770b082104f26af45dee90d1a2a8c0e8
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 01:13:24 +0530

    Added: maxFloat function which comapre which is max float value

--- GenericMaximum.java
+++ GenericMaximum.java
@@ -2,7 +2,8 @@ package generic.testmaximum;
 
 public class GenericMaximum
 {
-static Integer max;
+	static Integer maxInteger;
+	static Float maxFloat;
 	
 	/**
 	 * @param a
@@ -13,31 +14,57 @@ static Integer max;
 	 */
 	public static int maximumInteger(Integer a, Integer b, Integer c)
 	{
-		max = a;
+		maxInteger = a;
 		
-		if(b.compareTo(max) > 0)
+		if(b.compareTo(maxInteger) > 0)
 		{
-			max = b;
+			maxInteger = b;
 		}
-		if(c.compareTo(max) > 0)
+		if(c.compareTo(maxInteger) > 0)
 		{
-			max = c;
+			maxInteger = c;
 		}
-		return max;
+		return maxInteger;
+	}
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 * Finding max Float value using compareTo() method.
+	 */
+	public static Float maximumFloat(Float a, Float b, Float c)
+	{
+		maxFloat = a;
+		
+		if(b.compareTo(maxFloat) > 0)
+		{
+			maxFloat = b;
+		}
+		if(c.compareTo(maxFloat) > 0)
+		{
+			maxFloat = c;
+		}
+		return maxFloat;
 	}
 	
 	
 	/**
 	 * @param args
-	 * Passing integer value to function.
-	 * Calling maximumInteger function.
+	 * Passing integer and float value to function.
+	 * Calling functions.
 	 * printing the maximum number as a result.
 	 */
 	public static void main(String[] args)
 	{
 		Integer intNumeOne = 15, intNumTwo = 55, intNumThree = 24;
+		Float floatNumOne = 218.13f, floatNumTwo = 555.453f, floatNumThree = 999.999f;  
 		
 		maximumInteger(intNumeOne, intNumTwo, intNumThree);
-		System.out.println("Maximum amoungst : " + intNumeOne + " " + intNumTwo + " " + intNumThree + " is : " + max);
+		System.out.println("Maximum amoungst : " + intNumeOne + " " + intNumTwo + " " + intNumThree + " is : " + maxInteger + "\n");
+		
+		maximumFloat(floatNumOne, floatNumTwo, floatNumThree);
+		System.out.println("Maximum amoungst : " + floatNumOne + " " + floatNumTwo + " " + floatNumThree + " is : " + maxFloat);
 	}
 }
