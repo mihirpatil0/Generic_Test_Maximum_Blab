a6f79d3b251c447e48ead052570a7c36f1671049
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 00:55:17 +0530

    Display: Welcome message.

---
+++ GenericMaximum.java
@@ -0,0 +1,9 @@
+package generic.testmaximum;
+
+public class GenericMaximum
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Generic Maximum Problem Computation.");
+	}
+}
