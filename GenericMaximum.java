b5b0f049e8ae48cd1e6ccb9adcaaebfcb2b4b125
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 00:57:13 +0530

    Print: Maximum integer using compareTo method

--- GenericMaximum.java
+++ GenericMaximum.java
@@ -2,8 +2,42 @@ package generic.testmaximum;
 
 public class GenericMaximum
 {
+static Integer max;
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 * Finding max integer using compareTo() method.
+	 */
+	public static int maximumInteger(Integer a, Integer b, Integer c)
+	{
+		max = a;
+		
+		if(b.compareTo(max) > 0)
+		{
+			max = b;
+		}
+		if(c.compareTo(max) > 0)
+		{
+			max = c;
+		}
+		return max;
+	}
+	
+	
+	/**
+	 * @param args
+	 * Passing integer value to function.
+	 * Calling maximumInteger function.
+	 * printing the maximum number as a result.
+	 */
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome To Generic Maximum Problem Computation.");
+		Integer intNumeOne = 15, intNumTwo = 55, intNumThree = 24;
+		
+		maximumInteger(intNumeOne, intNumTwo, intNumThree);
+		System.out.println("Maximum amoungst : " + intNumeOne + " " + intNumTwo + " " + intNumThree + " is : " + max);
 	}
 }
